878e0bd8d761a468289da8c1cc5bb98df26eaaf1
Sundar Krishnan <sundar@Sundars-MacBook-Pro.local>
2021-08-03 09:21:07 -0400

    coffee stack

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'udacity-coffee-stack.us', // the auth0 domain prefix
+    audience: 'drinks', // the audience set for the auth0 app
+    clientId: '2lu4mLSEou1chqkFS8EcC3dz3WMvjo6n', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
